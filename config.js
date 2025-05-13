1ab4d4256ae489a44c4c30a8617885b4dc827bdc
Manmitha96 <manmithahemdinu@gmail.com>
2025-05-14 01:18:14 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "GFJlFA7b#5b9nYeV6ZKen3_wFwKgks3ax7YFCvkxQO2KmPFRrbtc",
+  SESSION_ID: process.env.SESSION_ID || "fBBIBSyK#quD9TMYjjVxVbLB6lmSle0-OwSn7eEEw3Tn3f4IFzk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:cKqwrneAPMeWgHattnWIDIYjSSpkEMwS@shortline.proxy.rlwy.net:15034",
   OWNER_NUM: process.env.OWNER_NUM || "94726939427",
 };
