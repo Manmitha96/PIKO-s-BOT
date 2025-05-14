5aee41527bb8132936bcae9f769c45e9d617f892
Manmitha96 <manmithahemdinu@gmail.com>
2025-05-14 23:24:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "qZ4U2TqB#0FlcB-Uf-5VPThAyJvFvh4iX9B-gD2DIjAlm_ptdlss",
   OWNER_NUM: process.env.OWNER_NUM || "94726939427",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manmitha96/BOT-PHOTOS/refs/heads/main/2025051419391432.jpg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n*☯️OWNED BY P_I_K_O☯️*",
+  MODE: process.env.MODE || "public",
 };
