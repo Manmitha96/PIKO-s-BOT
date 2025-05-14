35ead8fdb95af45be5d209106c0cbc1e0879c0ef
Manmitha96 <manmithahemdinu@gmail.com>
2025-05-14 22:46:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "qZ4U2TqB#0FlcB-Uf-5VPThAyJvFvh4iX9B-gD2DIjAlm_ptdlss",
   OWNER_NUM: process.env.OWNER_NUM || "94726939427",
+  PREFIX: process.env.PREFIX || ".",
 };
