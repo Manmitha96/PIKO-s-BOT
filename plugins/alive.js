af4ef6d3125118137694ddfb312aa0fa49d8a216
Manmitha96 <manmithahemdinu@gmail.com>
2025-05-16 20:15:44 +0900

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,6 +10,8 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+    await robin.sendPresenceUpdate('recording', from);
+    await robin.sendMessage(from, { audio: { url: "https://github.com/Manmitha96/BOT-PHOTOS/raw/refs/heads/main/BOT-AUDIO/alivevoice.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
     
 }catch(e){
 console.log(e)
