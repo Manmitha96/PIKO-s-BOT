d5e151d46947db57ff7c26af51c46192356d0b6a
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-07 20:08:16 +0530

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n> *by Asᴍᴏᴅᴇᴜs Eᴘᴢɪ*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n> *By Piko Blaze*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error fetching anime girl image: ${e.message}`);
