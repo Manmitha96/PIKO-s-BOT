43e852f45a5963291e914704484ccd7ecbff74f1
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-07 20:04:52 +0900

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> Asᴍᴏᴅᴇᴜs Eᴘᴢɪ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> Piko Baze' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error fetching dog image: ${e.message}`);
