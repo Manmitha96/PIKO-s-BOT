37026c37da0fdf282c27b1aba9e7c50dc46e542d
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-08 16:56:55 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -8,14 +8,19 @@ cmd({
     category: "owner",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const {exec} = require("child_process")
-reply("*restarting...*")
-await sleep(1500)
-exec("pm2 restart all")
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
+async(conn, mek, m,{
+    from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        if (!isOwner) {
+            return reply("*Only the bot owner can use this command.🕋*");
+        }
+        const {exec} = require("child_process")
+        reply("*restarting...*")
+        await sleep(1500)
+        exec("pm2 restart all")
+    } catch(e) {
+        console.log(e)
+        reply(`${e}`)
+    }
 })
