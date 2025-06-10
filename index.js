a7166ba826d895a6a9b8522147d7986d2815c10f
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-10 19:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ async function connectToWA() {
     const botNumber = robin.user.id.split(":")[0];
     const pushname = mek.pushName || "Sin Nombre";
     const isMe = botNumber.includes(senderNumber);
-    const isOwner = senderNumber.includes("94726939427") || isMe;
+    const isOwner = ownerNumber.includes(senderNumber)  isMe;
     const botNumber2 = await jidNormalizedUser(robin.user.id);
     const groupMetadata = isGroup
       ? await robin.groupMetadata(from).catch((e) => {})
@@ -161,6 +161,7 @@ async function connectToWA() {
     const groupAdmins = isGroup ? await getGroupAdmins(participants) : "";
     const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false;
     const isAdmins = isGroup ? groupAdmins.includes(sender) : false;
+    const isOwnergrp = isGroup && ownerNumber.includes((mek.key.participant || "").split("@")[0]);
     const isReact = m.message.reactionMessage ? true : false;
     const reply = (teks) => {
       robin.sendMessage(from, { text: teks }, { quoted: mek });
