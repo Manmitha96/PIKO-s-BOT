4aa9b2cd897eeb351da5a25c7d440c91083f4f9b
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-11 23:26:37 +0530

    Create jaicheck.js

---
+++ plugins/jaicheck.js
@@ -0,0 +1,16 @@
+const { cmd } = require('../command');
+const { exec } = require('child_process');
+const fs = require("fs");
+
+// 7. Get Bot JID
+cmd({
+    pattern: "jid",
+    desc: "Get the bot's JID.",
+    category: "owner",
+    react: "🤖",
+    filename: __filename
+},
+async (conn, mek, m, { from, isOwner, reply }) => {
+    if (!isOwner) return reply("❌ You are not the owner!");
+    reply(`🤖 *Bot JID:* ${conn.user.jid}`);
+});
