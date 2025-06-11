c5961ce509afa093b10e36d34f2176461bddb244
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-11 22:17:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -152,14 +152,15 @@ async function connectToWA() {
     const pushname = mek.pushName || "Sin Nombre";
     const isMe = botNumber.includes(senderNumber);
     const isOwner = ownerNumber.includes(senderNumber);
-    const botNumber2 = await (robin.user.id)
+    const botNumber2 = await jidNormalizedUser(robin.user.id)
+    const normalizedAdmins = groupAdmins.map(admin => jidNormalizedUser(admin));
     const groupMetadata = isGroup
       ? await robin.groupMetadata(from).catch((e) => {})
       : "";
     const groupName = isGroup ? groupMetadata.subject : "";
     const participants = isGroup ? await groupMetadata.participants : "";
     const groupAdmins = isGroup ? await getGroupAdmins(participants) : "";
-    const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false;
+    const isBotAdmins = isGroup ? normalizedAdmins.includes(botNumber2) : false;
     const isAdmins = isGroup ? groupAdmins.includes(sender) : false;
     const isReact = m.message.reactionMessage ? true : false;
     const reply = (teks) => {
