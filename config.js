d55b6a73c21290c12def4786701d02f3a4ddc6fa
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-11 23:21:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "OQ4xDAyB#cwefJ-TCRJB64Q4SEu-aXn4gFzpuLKwazX_JNdYyIaM",
-  OWNER_NUM: process.env.OWNER_NUM || "94726939427,75227023323273",
+  OWNER_NUM: process.env.OWNER_NUM || "94726939427,",
   OWNER_JID: process.env.OWNER_JID || "75227023323273@lid",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manmitha96/BOT-PHOTOS/refs/heads/main/2025051419391432.jpg",
