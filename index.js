94e8536d3e47ea29d78ffe99b57c6390cf65c600
Manmitha96 <manmithahemdinu@gmail.com>
2025-06-11 20:22:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ async function connectToWA() {
     const pushname = mek.pushName || "Sin Nombre";
     const isMe = botNumber.includes(senderNumber);
     const isOwner = ownerNumber.includes(senderNumber);
-    const botNumber2 = await jidNormalizedUser(robin.user.id);
+    const botNumber2 = robin.user.id.split('@')[0] + '@lid';
     const groupMetadata = isGroup
       ? await robin.groupMetadata(from).catch((e) => {})
       : "";
